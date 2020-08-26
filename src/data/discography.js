a3894acc1b76f78305f5b834395d9bbb3e7387ef
Dave Cohen <scraggo@gmail.com>
2020-08-25 21:11:59 -0500

    Update discovery page with new album

--- src/data/discography.js
+++ src/data/discography.js
@@ -3,20 +3,70 @@ export const solo = {
   urls: [
     {
       host: 'Bandcamp',
-      url: 'https://scraggo.bandcamp.com/releases',
+      url: 'https://scraggo.bandcamp.com',
     },
     {
       host: 'Spotify',
       url: 'https://open.spotify.com/artist/5V17hkb42RSTUJwDgrtCP2',
     },
+    {
+      host: 'ITunes',
+      url: 'https://itunes.apple.com/us/artist/1201153474',
+    },
+    {
+      host: 'Youtube Music',
+      url:
+        'https://music.youtube.com/playlist?list=OLAK5uy_nv50jikt5nqiIUHHNADq0HLyzr2tjnUj0',
+    },
+    {
+      host: 'Google Play',
+      url:
+        'https://play.google.com/store/music/artist/Scraggo?id=Anb7j2zzc3s646w5ya65jci5dia',
+    },
+    {
+      host: 'Amazon',
+      url: 'https://www.amazon.com/s?k=Scraggo&i=digital-music',
+    },
   ],
   albums: [
+    {
+      title: 'Identity',
+      urls: [
+        {
+          host: 'Bandcamp',
+          url: 'https://scraggo.bandcamp.com/album/identity',
+        },
+        {
+          host: 'Spotify',
+          url: 'https://open.spotify.com/album/3cIyTprx4o14cNE4FdyovV',
+        },
+        {
+          host: 'ITunes',
+          url: 'https://music.apple.com/us/album/identity/1528911344',
+        },
+        {
+          host: 'Youtube Music',
+          url:
+            'https://music.youtube.com/playlist?list=OLAK5uy_nv50jikt5nqiIUHHNADq0HLyzr2tjnUj0',
+        },
+        {
+          host: 'Google Play',
+          url:
+            'https://play.google.com/store/music/album/Scraggo_Identity?id=Bohkrai5yptmuwyammw7skwlh4e',
+        },
+        {
+          host: 'Amazon',
+          url:
+            'https://www.amazon.com/Identity-Scraggo/dp/B08GJZNJR8/ref=sr_1_1?dchild=1&keywords=scraggo&qid=1598364609&sr=8-1',
+        },
+      ],
+    },
     {
       title: 'Creative Being',
       urls: [
         {
           host: 'Bandcamp',
-          url: 'https://scraggo.bandcamp.com/releases',
+          url: 'https://scraggo.bandcamp.com/album/creative-being',
         },
         {
           host: 'Spotify',
--- src/pages/music/discography/index.js
+++ src/pages/music/discography/index.js
@@ -6,11 +6,22 @@ import SEO from 'src/components/SEO';
 
 import { solo, collabs } from 'src/data/discography';
 
-const getLink = linkData => {
-  const { host, url } = linkData;
+import albumImg from './identity-album-cover.png';
+import albumImgBack from './identity-back-cover.png';
+import './style.scss';
+
+const featuredAlbumLinks = solo.albums[0].urls;
+
+const getMusicServiceIcon = serviceName => {
+  const filename = serviceName.split(' ')[0].toLowerCase();
+  return <img src={`/music-service-icons/${filename}.png`} alt={serviceName} />;
+};
+
+const getLink = (linkData, className) => {
+  const { host, url, useIcon } = linkData;
   return (
-    <ExternalLink key={url} url={url} style={{ marginLeft: 4 }}>
-      {host}
+    <ExternalLink className={className} key={url} url={url}>
+      {useIcon && getMusicServiceIcon(host)} {host}
     </ExternalLink>
   );
 };
@@ -21,14 +32,15 @@ const getArtistDiscog = artistData => {
     <div>
       <h4>
         {`${artist}'s releases on`}
-        {urls.map(getLink)}
+        {urls.map(linkData => getLink(linkData, 'album-link'))}
       </h4>
       <ul>
         {albums.map(album => {
           const { title, urls } = album;
           return (
             <li>
-              {title} -{urls.map(getLink)}
+              {title}{' '}
+              {urls.map(linkData => getLink(linkData, 'album-link-small'))}
             </li>
           );
         })}
@@ -38,8 +50,24 @@ const getArtistDiscog = artistData => {
 };
 
 export default () => (
-  <Layout className="text-wrapper">
+  <Layout className="text-wrapper music-discography-page">
     <SEO title="Music Discography" />
+    <div className="feature">
+      <img className="featured-image" src={albumImg} alt="scraggo, Identity" />
+      <img
+        className="featured-image-back"
+        src={albumImgBack}
+        alt="scraggo, Identity, back cover"
+      />
+      <div className="featured-links flex flex-wrap flex-jc-center">
+        {featuredAlbumLinks.map(linkData =>
+          getLink(
+            { ...linkData, useIcon: true },
+            'album-link-button flex flex-ai-center'
+          )
+        )}
+      </div>
+    </div>
     <h1>Discography</h1>
     {getArtistDiscog(solo)}
     <h2>Collaborations</h2>
