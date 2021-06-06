33ef3b3a6817b75b876a82f7c736a2331d566d5f
Dave Cohen <scraggo@gmail.com>
2021-06-06 15:26:53 -0500

    Add book list

---
+++ src/data/book-list.js
@@ -0,0 +1,41 @@
+/** Schema */
+/*
+{
+  author: '',
+  description: '',
+  title: '',
+  genre: '',
+  readYear: 2021, // year I discovered/read the book
+  reviewLink: '', // internal link, if any
+  links: {
+    buy: '', // amazon, etc
+    notes: '', // external notes, summaries
+  }
+}
+*/
+
+export const bookList = [
+  // {
+  //   author: '',
+  //   title: '',
+  //   description: '',
+  //   readYear: 2021,
+  //   reviewLink: '',
+  //   links: {
+  //     buy: '',
+  //     notes: '',
+  //   }
+  // },
+  {
+    author: 'Robert Holden and Louise Hay',
+    description: null,
+    title: 'Life Loves You',
+    genre: 'self-help',
+    readYear: 2021,
+    reviewLink: null,
+    links: {
+      buy: null,
+      notes: null,
+    },
+  },
+];
